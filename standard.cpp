25029fcfd4d0f1813942f4d241d9c217924c7273
Satvik Kukreti <skukret1@cs.binghamton.edu>
2024-02-20 16:39:56 -0500

    Added line reading functonalities

--- standard.cpp
+++ standard.cpp
@@ -1,7 +1,35 @@
+#include <string>
 #include <iostream>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
-int main(){
-  cout << "Hello World\n";
+map<string, unsigned int> mmap;
+
+int main(int argv, char *argc[]){
+  if(argv != 3){
+    cerr << "standard <inputFile> <outputFile>\n";
+    return 1;
+  }
+  // Open the file
+  ifstream file(argc[1]);
+
+  // Check if the file is successfully opened
+  if (!file.is_open()) {
+      cerr << "Error opening file: " << argc[1] << endl;
+      return 1;
+  }
+
+    // Read and parse the file line by line
+    string line;
+    while (getline(file, line)) {
+        // Process each line as needed
+       cout << "Line: " << line << endl;
+    }
+
+    // Close the file
+    file.close();
+
+    return 0;
 }
